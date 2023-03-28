9f891d8a958d2996f9dd354e6d7cf57f8a7db19c
BirukL137 <buraf13k@gmail.com>
2023-03-28 14:50:51 +0300

    8. Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,7 @@
 export default function cleanSet(set, startString) {
-  const result = [];
-
   if (typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) return '';
 
-  for (const item of set) {
-    if (item.startsWith(startString)) {
-      result.push(item.slice(startString.length));
-    }
-  }
-  return result.join('-');
+  const filteredList = [...set].filter((word) => word && word.startsWith(startString));
+
+  return filteredList.map((word) => word.slice(startString.length)).join('-');
 }
