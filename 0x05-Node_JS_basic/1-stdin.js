a443a71eae469212b51529898afa6254c034ee57
BirukL137 <buraf13k@gmail.com>
2023-05-15 22:34:06 +0300

    1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,14 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n')
-process.stdin.setEncoding('utf8')
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.setEncoding('utf8');
+
 process.stdin.on('readable', () => {
-  const readline = process.stdin.read()
+  const readline = process.stdin.read();
   if (readline !== null) {
-    process.stdout.write(`Your name is: ${readline}`)
+    process.stdout.write(`Your name is: ${readline}`);
   }
-})
+});
 
 process.stdin.on('end', () => {
-  process.stdout.write('This import software is now closing\n')
+  process.stdout.write('This import software is now closing\n');
 })
