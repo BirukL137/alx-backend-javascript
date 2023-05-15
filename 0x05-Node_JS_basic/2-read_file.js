ee9f9403946d969cd5268a475d85a65c4d3a1f4c
BirukL137 <buraf13k@gmail.com>
2023-05-16 00:57:18 +0300

    2. Reading a file synchronously with Node JS

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+function countStudents(path) {
+  let database;
+  try {
+    database = fs.readFileSync(path);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  database = database.toString().split('\n');
+  let sts = database.filter((item) => item);
+  sts = sts.map((item) => item.split(','));
+  console.log(`Number of students: ${sts.length - 1}`);
+
+  const cs = [];
+  const swe = [];
+  for (const i in sts) {
+    if (sts[i][3] == 'CS') {
+      cs.push(sts[i][0]);
+    } else if (sts[i][3] == 'SWE') {
+      swe.push(sts[i][0]);
+    }
+  }
+
+  console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+  console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+}
+
+module.exports = countStudents;
