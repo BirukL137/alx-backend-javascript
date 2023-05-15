2541c019362d2de511599b3e33ee75a5177466ac
BirukL137 <buraf13k@gmail.com>
2023-05-16 01:41:49 +0300

    3. Reading a file asynchronously with Node JS

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,5 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
@@ -11,6 +10,6 @@ const app = http.createServer((req, res) => {
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-})
+});
 
 module.exports = app;
