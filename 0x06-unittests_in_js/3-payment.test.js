db2b748b0fc0676a80e59667a3df3c21b96db3ae
BirukL137 <buraf13k@gmail.com>
2023-05-18 10:52:33 +0300

    3. Spies

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -5,24 +5,16 @@ const sendPaymentRequestToApi = require('./3-payment');
 
 
 describe('sendPaymentRequestToApi', () => {
-  let spy;
-
-  beforeEach(() => {
-    spy = sinon.spy(Utils, 'calculateNumber')
-  });
-
-  afterEach(() => {
-    spy.restore();
-  });
-
   it('should call Utils.calculateNumber with the correct arguments', () => {
-    sendPaymentRequestToApi(100, 20);
-    chai.expect(spy).to.have.been.calledWith('SUM', 100, 20);
-  });
+    const spy_calc = sinon.spy(Utils, 'calculateNumber');
+    const console_spy = sinon.spy(console, 'log');
 
-  it('should return the same value as Utils.calculateNumber', () => {
-    const expectedResult = Utils.calculateNumber('SUM', 100, 20);
-    const actualResult = sendPaymentRequestToApi(100, 20);
-    chai.expect(actualResult).to.equal(expectedResult);
+    sendPaymentRequestToApi(100, 20);
+    
+    chai.expect(spy_calc.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
+    chai.expect(console_spy.calledWithExactly('The total is: 120')).to.be.true;
+
+    spy_calc.restore();
+    console_spy.restore();
   });
 });
